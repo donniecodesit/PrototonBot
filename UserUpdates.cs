e6dfa040e22a67b6b4e68168c1b829e2af7d2aed
Donovan Laws <donovan.laws@gmail.com>
2022-01-08 01:36:34 -0700

    UserUpdates revised!

--- UserUpdates.cs
+++ UserUpdates.cs
@@ -9,47 +9,43 @@ namespace PrototonBot
   public class UserUpdates
   {
     private static Random random = new Random();
-
-    public static Task MinuteRewards(IUser author)
-    {
+    //Handles giving the user Money and XP for talking, with a 1 minute cooldown.
+    public static Task MinuteRewards(IUser author) {
       var user = MongoHelper.GetUser(author.Id.ToString()).Result;
       var currentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
-      if (user.LastMessage > (currentTime - 60))
-      {
-        //User has received XP and Money within the last minute, do not repeat.
-        return Task.CompletedTask;
-      }
-      //Update the User's Money
-      var currentMoney = user.Money;
+      //If the user received MinuteRewards within the last 60 seconds, skip this task.
+      if (user.LastMessage > (currentTime - 60)) return Task.CompletedTask;
+      
+      //Update the user's money. Multiply if mutual partners and/or boosted user.
       double moneyToSet = random.Next(1, 8);
       if (user.Mutuals && user.Boosted) moneyToSet = Math.Round(moneyToSet * 1.1);
       else if (user.Mutuals || user.Boosted) moneyToSet = Math.Round(moneyToSet * 1.05);
-      moneyToSet += currentMoney;
+      moneyToSet += user.Money;
 
-      var currentExp = user.EXP;
+      //Update the user's EXP. Multiply if mutual partners and/or boosted user.
       double expToSet = random.Next(10, 21);
       if (user.Mutuals && user.Boosted) expToSet = Math.Round(expToSet * 1.1);
       else if (user.Mutuals || user.Boosted) expToSet = Math.Round(expToSet * 1.05);
-      expToSet += currentExp;
+      expToSet += user.EXP;
 
+      //Update the user in the database with the new information.
       MongoHelper.UpdateUser(author.Id.ToString(), "Money", moneyToSet);
       MongoHelper.UpdateUser(author.Id.ToString(), "EXP", expToSet);
       MongoHelper.UpdateUser(author.Id.ToString(), "LastMessage", currentTime);
       return Task.CompletedTask;
     }
 
-    public static Task LevelUpdater(SocketUserMessage message)
-    {
+    //Handles update the user's level based on a mathematical formula.
+    public static Task LevelUpdater(SocketUserMessage message) {
       var user = MongoHelper.GetUser(message.Author.Id.ToString()).Result;
-      var guild = MongoHelper.GetServer((message.Author as SocketGuildUser).Guild.Id.ToString()).Result;
+      var server = MongoHelper.GetServer((message.Author as SocketGuildUser).Guild.Id.ToString()).Result;
       long currentLevel = (long) Math.Floor((170 + Math.Sqrt(28900 - (6 * 310 * -user.EXP))) / 620);
-      if (currentLevel != user.Level)
-      {
-        if (guild.Id != "264445053596991498" && guild.EnabledChannels.Contains(message.Channel.Id.ToString()) && guild.LevelUpMessages)
-        {
+      if (currentLevel != user.Level) {
+        //Only reply if the server is not the TOP.GG server, the current channel is enabled, and level up messages are enabled.
+        if (server.Id != "264445053596991498" && server.EnabledChannels.Contains(message.Channel.Id.ToString()) && server.LevelUpMessages) {
           message.Channel.SendMessageAsync($":tada: **Congratulations {message.Author.Username}, you've reached Level {currentLevel}!** :tada:");
         }
+        //Regardless of if a reply was sent, update the user's level.
         MongoHelper.UpdateUser(message.Author.Id.ToString(), "Level", currentLevel);
       }
       return Task.CompletedTask;
