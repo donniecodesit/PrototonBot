54e9b1bf2469cb34c3f850afef7ca850853ecbc7
Donovan Laws <donovan.laws@gmail.com>
2022-01-09 01:59:36 -0700

    Revised StoreCommands and added conditional text

--- Commands/StoreCommands.cs
+++ Commands/StoreCommands.cs
@@ -6,61 +6,37 @@ using Discord.Commands;
 using Discord.WebSocket;
 using PrototonBot.MongoUtil;
 
-namespace PrototonBot.Commands
-{
-  public class StoreCommands : ModuleBase<SocketCommandContext>
-  {
-    [Command("store")]
-    [Alias("shop")]
-    [Summary("haha this is test for store")]
-    public async Task ItemStore(string page = null)
-    {
+namespace PrototonBot.Commands {
+  public class StoreCommands : ModuleBase<SocketCommandContext> {
+    [Command("store")] [Alias("shop")] [Summary("haha this is test for store")]
+    public async Task ItemStore(string page = null) {
       var serverObj = MongoHelper.GetServer(Context.Guild.Id.ToString()).Result;
       var user = MongoHelper.GetUser(Context.User.Id.ToString()).Result;
-
-      if (page == null)
-      {
+      if (page == null) {
         var embed = new EmbedBuilder();
         embed.WithColor(0xB2A2F1);
         embed.WithTitle("PrototonBot Store Directory");
         embed.WithThumbnailUrl(Context.Guild.GetUser(Program.UserID).GetAvatarUrl());
         embed.WithDescription("PrototonBot has different store pages you can view! Which one do you want?");
-        embed.AddField($"{serverObj.Prefix}store 1", $"View personal and profile effects you can purchase.");
+        embed.AddField($"{serverObj.Prefix}store 1", $"View personal profile effects you can purchase.");
         embed.AddField($"{serverObj.Prefix}store 2", $"View inventory items you can purchase.");
         await Context.Channel.SendMessageAsync("", false, embed.Build());
         return;
       }
-      if (page == "1")
-      {
-        if (serverObj.Id == Program.MasterSvr)
-        {
-          var embed = new EmbedBuilder();
-          embed.WithColor(0xB2A2F1);
-          embed.WithTitle("PrototonBot Store - Page 1");
-          embed.WithThumbnailUrl(Context.Guild.GetUser(Program.UserID).GetAvatarUrl());
-          embed.WithFooter($"Replying to: {Context.User.Username}");
-          embed.AddField($"``{serverObj.Prefix}buy Boosted`` - 10,000 Protobucks", "Get a 5% multiplier on Chat EXP and Protobucks!");
-          embed.AddField($"``{serverObj.Prefix}buy DailyPat`` - 1,000 Protobucks", "Restore your daily pat so that you can give another one!");
-          embed.AddField("Your Protobucks", user.Money);
-          await Context.Channel.SendMessageAsync("", false, embed.Build());
-          return;
-        }
-        else
-        {
-          var embed = new EmbedBuilder();
-          embed.WithColor(0xB2A2F1);
-          embed.WithTitle("PrototonBot Store - Page 1");
-          embed.WithThumbnailUrl(Context.Guild.GetUser(Program.UserID).GetAvatarUrl());
-          embed.WithFooter($"Replying to: {Context.User.Username}");
-          embed.AddField($"``{serverObj.Prefix}buy Boosted`` - 10,000 Protobucks", "Get a 5% multiplier on Chat EXP and Protobucks!");
-          embed.AddField($"``{serverObj.Prefix}buy DailyPat`` - 1,000 Protobucks", "Restore your daily pat so that you can give another one!");
-          embed.AddField("Your Protobucks", user.Money);
-          await Context.Channel.SendMessageAsync("", false, embed.Build());
-          return;
-        }
+
+      if (page == "1") {
+        var embed = new EmbedBuilder();
+        embed.WithColor(0xB2A2F1);
+        embed.WithTitle("PrototonBot Store - Page 1");
+        embed.WithThumbnailUrl(Context.Guild.GetUser(Program.UserID).GetAvatarUrl());
+        embed.WithFooter($"Replying to: {Context.User.Username}");
+        embed.AddField($"``{serverObj.Prefix}buy Boosted`` - 10,000 Protobucks", "Get a 5% multiplier on Chat EXP and Protobucks!");
+        embed.AddField($"``{serverObj.Prefix}buy DailyPat`` - 1,000 Protobucks", "Restore your daily pat so that you can give another one!");
+        embed.AddField("Your Protobucks", user.Money);
+        await Context.Channel.SendMessageAsync("", false, embed.Build());
+        return;
       }
-      if (page == "2")
-      {
+      if (page == "2") {
         var embed = new EmbedBuilder();
         embed.WithColor(0xB2A2F1);
         embed.WithTitle("PrototonBot Store - Page 2");
@@ -75,140 +51,97 @@ namespace PrototonBot.Commands
       }
     }
 
-    [Command("buy")]
-    [Alias("purchase", "redeem", "get")]
-    public async Task BuyCommand(string item = null)
-    {
+    [Command("buy")] [Alias("purchase", "redeem", "get")]
+    public async Task BuyCommand(string item = null) {
       var serverObj = MongoHelper.GetServer(Context.Guild.Id.ToString()).Result;
       var user = MongoHelper.GetUser(Context.User.Id.ToString()).Result;
       var inv = MongoHelper.GetInventory(Context.User.Id.ToString()).Result;
 
-      if (item == null)
-      {
+      if (item == null) {
         await Context.Channel.SendMessageAsync($"Please enter a valid item to buy.\nUse {serverObj.Prefix}store to learn more.");
         return;
-      }
+      } 
 
-      if (item.ToLower() == "boosted")
-      {
-        if (user.Money >= 10000)
-        {
-          if (user.Boosted == true)
-          {
-            await Context.Channel.SendMessageAsync($"You already are boosted, <@{user.Id}>!");
-          }
-          else
-          {
+      if (item.ToLower() == "boosted") {
+        if (user.Money >= 10000) {
+          if (user.Boosted == true) await Context.Channel.SendMessageAsync($"You already are boosted, <@{user.Id}>!");
+          else {
             await Context.Channel.SendMessageAsync($"Congratulations! You're now boosted and had 10000 Protobucks taken from your bank. Enjoy! <@{user.Id}>");
             await MongoHelper.UpdateUser(user.Id, "Money", (user.Money - 10000));
             await MongoHelper.UpdateUser(user.Id, "Purchases", (user.Purchases + 1));
             await MongoHelper.UpdateUser(user.Id, "Boosted", true);
           }
-          return;
-        }
-        else
-        {
+        } else {
           await Context.Channel.SendMessageAsync($"Sorry, but you don't have enough Protobucks to afford this!\nYou have {user.Money}/10000 Protobucks.");
-          return;
         }
       }
 
-      if (item.ToLower() == "dailypat")
-      {
-        if (user.Money >= 1000)
-        {
+      else if (item.ToLower() == "dailypat") {
+        if (user.Money >= 1000) {
           var currentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
-          if (user.LastPat > (currentTime - 86400))
-          {
+          if (user.LastPat > (currentTime - 86400)) {
             await MongoHelper.UpdateUser(user.Id, "LastPat", 0);
             await MongoHelper.UpdateUser(user.Id, "Money", (user.Money - 1000));
             await MongoHelper.UpdateUser(user.Id, "Purchases", (user.Purchases + 1));
             await Context.Channel.SendMessageAsync($"Awesome! Your daily pat was reset and 1000 Protobucks taken from your bank. Enjoy, <@{user.Id}>!");
-            return;
-          }
-          else
-          {
+          } else {
             await Context.Channel.SendMessageAsync($"You haven't even given away your daily pat today, wouldn't that be a waste of Protobucks, <@{user.Id}>?");
-            return;
           }
-        }
-        else
-        {
+        } else {
           await Context.Channel.SendMessageAsync($"Sorry, but you don't have enough Protobucks to afford this!\nYou have {user.Money}/1000 Protobucks.");
-          return;
         }
       }
 
-      if (item.ToLower() == "axe")
-      {
-        if (user.Money >= 500)
-        {
+      else if (item.ToLower() == "axe") {
+        if (user.Money >= 500) {
           await MongoHelper.UpdateInventory(user.Id, "Axes", (inv.Axes + 1));
           if (inv.AxeUses == 0) await MongoHelper.UpdateInventory(user.Id, "AxeUses", 10);
           await MongoHelper.UpdateUser(user.Id, "Money", (user.Money - 500));
           await MongoHelper.UpdateUser(user.Id, "Purchases", (user.Purchases + 1));
-          await Context.Channel.SendMessageAsync($"Awesome, you've purchased an :axe: for 500 Protobucks! You now have {inv.Axes + 1} Axe(s)! <@{user.Id}>");
-          return;
-        }
-        else
-        {
+          await Context.Channel.SendMessageAsync($"Awesome, you've purchased an :axe: for 500 Protobucks! You now have {inv.Axes + 1} Axe{((inv.Axes + 1 == 1) ? "" : "s")}! <@{user.Id}>");
+        } else {
           await Context.Channel.SendMessageAsync($"Sorry, but you don't have enough Protobucks to afford this!\nYou have {user.Money}/500 Protobucks.");
-          return;
         }
       }
 
-      if (item.ToLower() == "pick")
-      {
-        if (user.Money >= 700)
-        {
+      else if (item.ToLower() == "pick") {
+        if (user.Money >= 700) {
           await MongoHelper.UpdateInventory(user.Id, "Picks", (inv.Picks + 1));
           if (inv.PickUses == 0) await MongoHelper.UpdateInventory(user.Id, "PickUses", 10);
           await MongoHelper.UpdateUser(user.Id, "Money", (user.Money - 700));
           await MongoHelper.UpdateUser(user.Id, "Purchases", (user.Purchases + 1));
-          await Context.Channel.SendMessageAsync($"Awesome, you've purchased a :pick: for 700 Protobucks! You now have {inv.Picks + 1} Pick(s)! <@{user.Id}>");
-          return;
-        }
-        else
-        {
+          await Context.Channel.SendMessageAsync($"Awesome, you've purchased a :pick: for 700 Protobucks! You now have {inv.Picks + 1} Pick{((inv.Picks + 1 == 1) ? "" : "s")}! <@{user.Id}>");
+        } else {
           await Context.Channel.SendMessageAsync($"Sorry, but you don't have enough Protobucks to afford this!\nYou have {user.Money}/700 Protobucks.");
-          return;
         }
       }
 
-      if (item.ToLower() == "wrench")
-      {
-        if (user.Money >= 900)
-        {
+      else if (item.ToLower() == "wrench") {
+        if (user.Money >= 900) {
           await MongoHelper.UpdateInventory(user.Id, "Wrenches", (inv.Wrenches + 1));
           if (inv.WrenchUses == 0) await MongoHelper.UpdateInventory(user.Id, "WrenchUses", 10);
-          await MongoHelper.UpdateUser(user.Id, "Money", (user.Money - 700));
+          await MongoHelper.UpdateUser(user.Id, "Money", (user.Money - 900));
           await MongoHelper.UpdateUser(user.Id, "Purchases", (user.Purchases + 1));
-          await Context.Channel.SendMessageAsync($"Awesome, you've purchased a :wrench: for 700 Protobucks! You now have {inv.Wrenches + 1} Wrench(es)! <@{user.Id}>");
-          return;
-        }
-        else
-        {
+          await Context.Channel.SendMessageAsync($"Awesome, you've purchased a :wrench: for 900 Protobucks! You now have {inv.Wrenches + 1} Wrench{((inv.Wrenches + 1 == 1) ? "" : "es")}! <@{user.Id}>");
+        } else {
           await Context.Channel.SendMessageAsync($"Sorry, but you don't have enough Protobucks to afford this!\nYou have {user.Money}/900 Protobucks.");
-          return;
         }
       }
 
-      await Context.Channel.SendMessageAsync($"Sorry, but the item you typed doesn't exist in our store. Please check the spelling or store and try again ^^ <@{user.Id}>");
+      else {
+        await Context.Channel.SendMessageAsync($"Sorry, but the item you typed doesn't exist in our store. Please check the spelling or store and try again ^^ <@{user.Id}>");
+      }
+
       return;
-
-
     }
 
     [Command("upgrade")]
-    public async Task UpgradeStore(string item = null)
-    {
+    public async Task UpgradeStore(string item = null) {
       var serverObj = MongoHelper.GetServer(Context.Guild.Id.ToString()).Result;
       var user = MongoHelper.GetUser(Context.User.Id.ToString()).Result;
       var inv = MongoHelper.GetInventory(Context.User.Id.ToString()).Result;
 
-      if (item == null || item == "shop")
-      {
+      if (item == null || item == "shop") {
         var embed = new EmbedBuilder();
         embed.WithColor(0xB2A2F1);
         embed.WithTitle("PrototonBot Upgrade Shop");
@@ -221,56 +154,42 @@ namespace PrototonBot.Commands
         return;
       }
 
-      if (item.ToLower() == "luck")
-      {
-        if (inv.DailyCoins >= 14 && inv.PatCoins >= 14 && inv.GambleCoins >= 50)
-        {
-          if (user.Luck >= 0 && user.Luck <= 23)
-          {
+      if (item.ToLower() == "luck")  {
+        if (inv.DailyCoins >= 14 && inv.PatCoins >= 14 && inv.GambleCoins >= 50) {
+          if (user.Luck >= 0 && user.Luck <= 23) {
             await Context.Channel.SendMessageAsync($"Wowwee! Look at all these coins, oh my gosh! That's amazing! <@{user.Id}>, thank you for this massive heap! You must have worked really hard for these, so I'll fuse them into your soul and give you an increase of 1 on your Luck, but keep in mind it maxes out at 25.");
             await MongoHelper.UpdateInventory(user.Id, "DailyCoins", (inv.DailyCoins - 14));
             await MongoHelper.UpdateInventory(user.Id, "PatCoins", (inv.PatCoins - 14));
             await MongoHelper.UpdateInventory(user.Id, "GambleCoins", (inv.GambleCoins - 50));
             await MongoHelper.UpdateUser(user.Id, "Luck", (user.Luck + 1));
-            return;
-
           }
-          if (user.Luck == 24)
-          {
+
+          else if (user.Luck == 24) {
             await Context.Channel.SendMessageAsync($"Wowwee! Look at all these coins, oh my gosh! That's amazing! <@{user.Id}>, thank you for this massive heap! You must have worked really hard for these, so I'll fuse them into your soul and give you an increase of 1 on your Luck-\n***OH MY GOSH WOW YOU REACHED THE MAXIMUM LUCK STAT OF 25! CONGRATULATIONS!***");
             await MongoHelper.UpdateInventory(user.Id, "DailyCoins", (inv.DailyCoins - 14));
             await MongoHelper.UpdateInventory(user.Id, "PatCoins", (inv.PatCoins - 14));
             await MongoHelper.UpdateInventory(user.Id, "GambleCoins", (inv.GambleCoins - 50));
             await MongoHelper.UpdateUser(user.Id, "Luck", (user.Luck + 1));
-            return;
           }
-          if (user.Luck == 25)
-          {
+
+          else if (user.Luck == 25) {
             await Context.Channel.SendMessageAsync($"<@{user.Id}>, I'm super sorry, but the Luck stat caps out at 25, and.. you're already there. Try upgrading other stats now!");
-            return;
           }
-        }
-        else
-        {
+        } else {
           await Context.Channel.SendMessageAsync($"Seems like you don't have enough coins. Here's what you have compared to what you need! <@{user.Id}>\nDC: ``{inv.DailyCoins}/14``, PC: ``{inv.PatCoins}/14``, ``{inv.GambleCoins}/50``.");
-          return;
         }
+        return;
       }
 
-      if (item.ToLower() == "dailybonus" || item.ToLower() == "daily")
-      {
-        if (inv.DailyCoins >= 7)
-        {
+      if (item.ToLower() == "dailybonus" || item.ToLower() == "daily") {
+        if (inv.DailyCoins >= 7) {
           await Context.Channel.SendMessageAsync($"Wowza, you really are devoted! I'll take these 7 Daily Coins from you and fuse them into your soul...\nNow you earn 100 more Protobucks on your dailies! <@{user.Id}>");
           await MongoHelper.UpdateInventory(user.Id, "DailyCoins", (inv.DailyCoins - 7));
           await MongoHelper.UpdateUser(user.Id, "DailyBonus", (user.DailyBonus + 100));
-          return;
-        }
-        else
-        {
+        } else {
           await Context.Channel.SendMessageAsync($"Seems like you don't have enough coins. Here's what you have compared to what you need! <@{user.Id}>\nDC: ``{inv.DailyCoins}/7``.");
-          return;
         }
+        return;
       }
     }
   }
